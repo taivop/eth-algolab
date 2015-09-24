4c58ecc346b5c0c9de73d394714648c51bd92e10
taivop <taivo.pungas@gmail.com>
2015-09-24 10:34:41 +0200

    #9 40 points WA OK TL

---
+++ 2-aliens/first.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void test_case() {
+	int m, n;
+
+	cin >> n;
+	cin >> m;
+
+	vector<int> p, q;
+	// pre-allocate
+	p.reserve(n);
+	q.reserve(n);
+
+	for(int i=0; i<n; i++) {
+		cin >> p[i];
+		cin >> q[i];
+	}
+
+
+	int counter=0;
+	for(int i=0; i<n; i++) {
+		if(p[i] == 0)
+			continue;
+		else {
+			bool has_superinterval = false;
+			for(int j=0; j<n; j++) {
+				if((p[j] < p[i] && q[j] >= q[i]) || (p[j] <= p[i] && q[j] > q[i])) {
+					has_superinterval = true;
+					break;
+				}
+			}
+
+			if(!has_superinterval)
+				counter += 1;
+		}
+	}
+
+	cout << counter << endl;
+}
+
+int main(void) {
+	std::ios_base::sync_with_stdio(false);
+	
+	int N;
+	cin >> N;
+
+	for(int i=0; i<N; i++)
+		test_case();
+
+	return 0;
+}
