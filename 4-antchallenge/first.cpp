37f1eb8e1442a59f6337fd1eff8976fcc3894ed4
taivop <taivo.pungas@gmail.com>
2015-10-13 18:00:20 +0200

    #15 first approach and tests

--- 4-antchallenge/first.cpp
+++ 4-antchallenge/first.cpp
@@ -32,24 +32,29 @@ void test_case() {
 		int t1, t2, w;
 		cin >> t1 >> t2;	// <start_node> <end_node>
 
-		cout << "Adding edge: " << t1 << "->" << t2 << ". [";
+		//cout << "Adding edge: " << t1 << "--" << t2 << ", ";
 		int min_weight = 10001;
 		for(int j=0; j<s; j++) {
 			cin >> w;
-			cout << " " << w;
+			//cout << " " << w;
 
 			if(w < min_weight) {
-				cout << "<" << min_weight;
+				//cout << "<" << min_weight;
 				min_weight = w;
 			}
 		}
 
-		cout << "] w=" << min_weight << endl;
+		//cout << "w=" << min_weight << endl;
 
 		Edge e;
 		tie(e, tuples::ignore) = add_edge(t1, t2, G);
 		wm[e] = min_weight;
 	}
+
+	// Read in hive locations
+	int asd;
+	for(int i=0; i<s; i++)
+		cin >> asd;
 
 	// Get shortest path from start node
 	//Compute shortes t-u path in G_T
@@ -60,13 +65,20 @@ void test_case() {
 	    predecessor_map(make_iterator_property_map(pred.begin(), get(vertex_index, G))).
 	    distance_map(make_iterator_property_map(dist.begin(), get(vertex_index, G))));
 
-	for(int i=0; i<dist.size(); i++) {
-		/*if(pred[i] == a)
-			cout << "!!! " << pred[i] << ", distance " << dist[i] << endl;
-		else*/
-			cout << pred[i] << ", distance " << dist[i] << endl;
+	/*int dst = a;
+	int src = pred[a];
+	while(dst != b) {
+		cout << src << "--" << dst << ", distance " << dist[dst] << endl;
+		dst = src;
+		src = pred[dst];
 	}
-		//cout << pred[i] << " " << dist[i] << endl;
+
+	for(int i=0; i<dist.size(); i++) {
+		if(pred[i] == a)
+			cout << "!!! " << i << ", distance " << dist[i] << endl;
+		else
+			cout << i << "--" << b << ", distance " << dist[i] << endl;
+	}*/
 
 	// Q: Why do we even need the hive locations?
 	// A: Because each hive network is an MST of the graph with the limitation that the hive is on this MST.
@@ -74,7 +86,7 @@ void test_case() {
 
 	// Go through each MST and find m, the minimum path on that MST. Then find min(m) which is the answer. Constant time.
 
-	//cout << sum << " " << max_distance << endl;
+	cout << dist[a] << endl;
 	
 }
 
@@ -84,8 +96,10 @@ int main(void) {
 	int t;
 	cin >> t;
 
-	for(int i=0; i<t; i++)
+	for(int i=0; i<t; i++) {
+		//cout << "---- TEST CASE " << i << " ----" << endl;
 		test_case();
+	}
 
 	return 0;
 }
