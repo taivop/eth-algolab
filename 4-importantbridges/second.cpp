87665b2a0c08b831c45364281d1b36f3abe7ad86
taivop <taivo.pungas@gmail.com>
2015-10-19 18:53:53 +0200

    fix #16 CORRECT

--- 4-importantbridges/second.cpp
+++ 4-importantbridges/second.cpp
@@ -73,48 +73,31 @@ void test_case() {
 	   // eit is EdgeIterator, *eit is EdgeDescriptor
 	   int u = source(*eit, G), v = target(*eit, G);
 	   num_edges_in_component[component[*eit]] += 1;
-	   //cout << *eit << ": " << component[*eit] << endl;
+	   //cout << "component of edge " << *eit << ": " << component[*eit] << endl;
+	   //cout << *eit << ": " << component[*eit] << endl
 	}
 
-	// TODO count one-edge components!
-
 	vector<string> criticals;
+	vector<pair<int, int> > criticals2;
 
 	// Do second pass over all edges and get all edges that are in 1-edge components
 	for (tie(eit, eend) = edges(G); eit != eend; ++eit) {
 	   // eit is EdgeIterator, *eit is EdgeDescriptor
 	   	int u = source(*eit, G), v = target(*eit, G);
-		if(num_edges_in_component[component[*eit]] == 1)
-   			criticals.push_back("" + min(to_string(u), to_string(v)) + " " + max(to_string(u), to_string(v)));
+	   	//cout << "edges in component " << component[*eit] << ": " << num_edges_in_component[component[*eit]] << endl;
+		if(num_edges_in_component[component[*eit]] == 1) {
+   			criticals2.push_back(pair<int,int>(min(u, v), max(u, v)));
+   		}
 	}
 
 
-	sort(criticals);
-
-	/*// Remove duplicates
-	string last;
-	vector<string> no_duplicates;
-	for(int i=0; i<criticals.size(); i++) {
-			if(i == 0) {
-				no_duplicates.push_back(criticals[i]);
-			} else {
-				last = criticals[i-1];
-				if(last != criticals[i]) {
-					no_duplicates.push_back(criticals[i]);
-					last = criticals[i];
-				}
-
-			}
-		}*/
+	sort(criticals2);
 
 	// Print results
-	
-
-	// Print results TODO replace num_comps with number of one-edge components!
-	if(num_comps > 1) {
-		cout << num_comps << endl;
-		for(int i=0; i<criticals.size(); i++) {
-			cout << criticals[i] << endl;
+	if(criticals2.size() >= 1) {
+		cout << criticals2.size() << endl;
+		for(int i=0; i<criticals2.size(); i++) {
+			cout << criticals2[i].first << " " << criticals2[i].second << endl;
 		}
 	} else {
 		cout << "0" << endl;
@@ -129,7 +112,7 @@ int main(void) {
 	cin >> t;
 
 	for(int i=0; i<t; i++) {
-		cout << "---- NEW TESTCASE ----" << endl;
+		//cout << "---- NEW TESTCASE ----" << endl;
 		test_case();
 	}
 
