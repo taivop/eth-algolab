fb0d05dfe0e6b5c1f758a9b52ee4532c50f718df
taivop <taivo.pungas@gmail.com>
2015-11-08 15:56:34 +0100

    #28 base

---
+++ 8-graypes/first.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void test_case() {
+	
+}
+
+int main(void) {
+	std::ios_base::sync_with_stdio(false);
+	
+	int N;
+	cin >> N;
+
+	for(int i=0; i<N; i++)
+		test_case();
+
+	return 0;
+}
