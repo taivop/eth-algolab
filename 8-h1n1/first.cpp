8ac9eda01e3958b12de6d06a29d00079df4fc0cd
taivop <taivo.pungas@gmail.com>
2015-11-11 14:29:04 +0100

    #30 recursion compiles and runs but for some reason, the triangulation is messed up -- no infinite edges are found

--- 8-h1n1/first.cpp
+++ 8-h1n1/first.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
@@ -18,6 +19,52 @@ typedef Triangulation::Vertex_handle  Vertex_handle;
 
 using namespace std;
 
+bool recurse(Face_handle current_face, long d, vector<Face_handle>& visited, Triangulation t) {
+
+	// Remember we visited this node
+	visited.push_back(current_face);
+
+	cout << "Recursion at: " << t.dual(current_face) << endl;
+
+	// Base of recursion: check if we are free
+	if(t.is_infinite(current_face)) {
+		cout << "Infinite face!" << endl;
+		return true;
+	}
+
+	for(int neighbor_num=0; neighbor_num<3; neighbor_num++) {
+		Face_handle neighbor_face = current_face->neighbor(neighbor_num);
+		//cout << (current_face == neighbor_face) << endl;
+		cout << "\tChecking neighbor of vertex " << current_face->vertex(neighbor_num)->point() << endl;
+
+		cout << "\tPoints: ";
+		for(int j=0; j<3; j++) {
+			cout << neighbor_face->vertex(j)->point() << ", ";
+		}
+		cout << endl;
+
+		// If we already visited
+		if(find(visited.begin(), visited.end(), current_face) != visited.end()) {
+			continue;
+		}
+
+		Vertex_handle border_endpoint1 = current_face->vertex((neighbor_num + 1) % 3);
+		Vertex_handle border_endpoint2 = current_face->vertex((neighbor_num + 2) % 3);
+		K::FT border_length_sq = CGAL::squared_distance(border_endpoint1->point(), border_endpoint2->point());
+
+		if(CGAL::to_double(border_length_sq) >= 4 * d) {	// If we can fit through that edge 
+			// cout << "can fit through edge " << neighbor_num << endl;
+
+			// New search starting from neighbor
+			if(recurse(neighbor_face, d, visited, t)) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
 void test_case(int n) {
 
 	// cout << "---- " << n << " ----" << endl;
@@ -31,6 +78,7 @@ void test_case(int n) {
 		K::Point_2 p;
 		cin >> p;
 		infected.push_back(p);
+		cout << "Read in point " << p << endl;
 	}
 
 	// Construct Delauney triangulation
@@ -49,7 +97,6 @@ void test_case(int n) {
 		// --- Find an escape path for this person ---
 		// Find out at which face we are
 		Face_handle current_face = t.locate(escaper);
-		K::Point_2 current_voronoi = t.dual(current_face);
 
 		// Check if we are already outside
 		if(t.is_infinite(current_face)) {
@@ -67,22 +114,14 @@ void test_case(int n) {
 			continue;
 		}*/
 
-		// Check all incident faces if we could possibly go there
+		// Recurse
+		vector<Face_handle> visited;
+		bool result = recurse(current_face, d, visited, t);
 
-		vector<Face_handle> visited_faces;
-		visited_faces.push_back(current_face);
-
-		for(int neighbor_num=0; neighbor_num<3; neighbor_num++) {
-			Vertex_handle opposite_vertex  = current_face->vertex(neighbor_num);
-			Vertex_handle border_endpoint1 = current_face->vertex((neighbor_num + 1) % 3);
-			Vertex_handle border_endpoint2 = current_face->vertex((neighbor_num + 2) % 3);
-			K::FT border_length = CGAL::squared_distance(border_endpoint1->point(), border_endpoint2->point());
-			Edge border = pair<Face_handle, int>(current_face, neighbor_num);
-			Face_handle neighbor_face = current_face->neighbor(neighbor_num);
-			K::Point_2 neighbor_voronoi = t.dual(neighbor_face);
-		}
-
-
+		if(result)
+			cout << "POSSIBLE TO ESCAPE" << endl;
+		else
+			cout << "COULDN'T ESCAPE :(" << endl;
 	}
 
 }
@@ -100,3 +139,6 @@ int main(void) {
 
 	return 0;
 }
+
+// https://judge.inf.ethz.ch/doc/cgal/doc_html/Triangulation_2/classCGAL_1_1Triangulation__2.html#a37b3896ab3a416fe4adff475ed625e16
+// http://doc.cgal.org/latest/Kernel_23/classCGAL_1_1Point__2.html
